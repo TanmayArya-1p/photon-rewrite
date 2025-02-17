45d2fd4788fe5bb52ddcf6e7096fc46488905d67
TanmayArya <72544600+TanmayArya-1p@users.noreply.github.com>
2025-02-17 14:01:18 +0530

    before qr

--- app/(tabs)/session.jsx
+++ app/(tabs)/session.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView , Text , ActivityIndicator,View, TouchableOpacity} from "react-native";
+import { SafeAreaView , Text , ActivityIndicator,View, TouchableOpacity,Share } from "react-native";
 import {userStore} from "../stores/user"
 import { useEffect , useState } from "react";
 import {PebbleDispatcher} from "@/components/pebble/dispatcher"
@@ -7,6 +7,8 @@ import * as api from "@/components/pebble/api"
 import {pebbleStore , sessionStore} from "@/components/pebble/stores"
 import { Ionicons } from "@expo/vector-icons";
 import { Button } from "react-native-web";
+import * as Clipboard from 'expo-clipboard';
+
 
 export default function SessionPage(props) {
     const { user, setUser } = userStore()
@@ -14,12 +16,14 @@ export default function SessionPage(props) {
     const [albumName,  setAlbumName] = useState("Camera")
     const {pebbles: ps} = pebbleStore()
     const {sesID , sesKey } = sessionStore()
+    const [connectionString , setConnectionString] = useState("")
 
     useEffect(() => {
         if(user.is_alive == false) return
         api.login(user.pebble_uid , user.pebble_password).then(()=> {
             console.log("Logged in to PebbleDB" ,res)
         })
+        setConnectionString(sesID + "|" + sesKey + "|" + "lol.onetincan.tech")
         setDone(true)
     },[user])
 
@@ -54,25 +58,33 @@ export default function SessionPage(props) {
         </View>
         <View className="mt-5 bg-blue-950 p-2 flex-row rounded-tr-xl justify-between rounded-tl-xl w-[90%]">
             <Text className="text-white font-extrabold justify-center mt-1">Connection String</Text>
-            <TouchableOpacity className="bg-blue-500 rounded-md p-1 items-center justify-center">
-                <Text className="font-extrabold text-white mx-2">Copy</Text>
-            </TouchableOpacity>
+            <View className="flex-row">
+                <TouchableOpacity className="bg-blue-500 rounded-md mx-3 p-1 items-center justify-center">
+                    <Ionicons name="qr-code-outline" size={20} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => Clipboard.setStringAsync(connectionString)} className="bg-blue-500 rounded-md p-1 items-center justify-center">
+                    <Text className="font-extrabold text-white mx-2">Copy</Text>
+                </TouchableOpacity>
+            </View>
         </View>
+
         <View className="p-3 bg-blue-900 shadow-blue-900 shadow-2xl rounded-br-xl rounded-bl-xl w-[90%]">
-            <Text className="text-sm text-white">photon:{(sesID+"|"+sesKey+"|"+"lol.onetincan.tech").slice(0,35) + "..."}</Text>
+            <Text className="text-sm text-white">{connectionString.slice(0,50) + "..."}</Text>
         </View>
         <View className="mt-4 flex-row justify-between w-[90%]">
-            <TouchableOpacity className="flex-row items-center justify-center bg-slate-900 p-3 rounded-lg w-[49%]">
-                <Ionicons name="person-add-outline" className="mr-2" size={20} color="white" />
-                <Text className="text-center font-bold text-white">Invite Friends</Text>
+            <TouchableOpacity onPress={() => Share.share({
+                message: "Join My Session on Photon:\n" + connectionString + "\nphoton://join?=" + connectionString
+            })} className="flex-row items-center justify-center bg-slate-200 p-3 rounded-lg w-[49%]">
+                <Ionicons name="person-add-outline" className="mr-2" size={20} color="black" />
+                <Text className="text-center font-bold text-black">Invite Friends</Text>
             </TouchableOpacity>
-            <TouchableOpacity className="flex-row items-center justify-center bg-slate-900 p-2 rounded-lg w-[49%]">
-                <Ionicons name="qr-code-outline" className="mr-2" size={20} color="white" />
-                <Text className="text-center font-bold text-white">Show Invite QR</Text>
+            <TouchableOpacity className="flex-row items-center justify-center bg-red-100 p-2 rounded-lg w-[49%]">
+                <Ionicons name="exit-outline" className="mr-2" size={20} color="black" />
+                <Text className="text-center font-bold text-black">Leave</Text>
             </TouchableOpacity>
         </View>
 
-        {/* {done && <PebbleDispatcher album={albumName} interval={10000}/> }
+        {/* {done && <PebbleDispatcher album={albumName} interval={10000} />}
         {done && <RequestHandler album={albumName} poller_interval={10000} />} */}
     </SafeAreaView>
 }
